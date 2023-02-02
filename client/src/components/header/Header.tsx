bf59236923137263cf7cd34d85c42b30142b1392
LeonID <lmatskevich79@gmail.com>
2023-02-02 23:47:15 +0300

    feat: add header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,13 +1,48 @@
 import './header.scss';
+import { Search, Person, Chat, Notifications } from '@mui/icons-material';
 
 export default function Header() {
   return (
     <div className="header">
       <div className="container">
         <div className="headerWrapper">
-        <div className="headerLeft"><span className="logo">Social App</span></div>
-        <div className="headerCenter">search</div>
-        <div className="headerRight">user</div>
+          <div className="headerLeft">
+            <span className="logo">Social App</span>
+          </div>
+          <div className="headerCenter">
+            <div className="searchbar">
+              <Search className="searchIcon" />
+              <input
+                placeholder="Search for friend, post or video"
+                className="searchInput"
+              />
+            </div>
+          </div>
+          <div className="headerRight">
+            <div className="headerLinks">
+              <span className="headerLink">Homepage</span>
+              <span className="headerLink">Timeline</span>
+            </div>
+            <div className="headerIcons">
+              <div className="headerIconItem">
+                <Person />
+                <span className="headerIconBadge">1</span>
+              </div>
+              <div className="headerIconItem">
+                <Chat />
+                <span className="headerIconBadge">1</span>
+              </div>
+              <div className="headerIconItem">
+                <Notifications />
+                <span className="headerIconBadge">1</span>
+              </div>
+            </div>
+            <img
+              src="./assets/images/person/1.jpg"
+              alt="avatar"
+              className="headerImg"
+            />
+          </div>
         </div>
       </div>
     </div>
