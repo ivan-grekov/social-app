0b70ccbaaad09367a577fcebbaa56102a03f55cd
Alexandr <tribisalexandr@yandex.ru>
2023-02-02 23:31:06 +0300

    feat: add profile page layout

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import './home.scss';
 import  Header  from '../../components/header/Header';
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return <Header />;
 }
--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,20 +1,26 @@
 import "./profile.scss";
 import userPhoto from '../../assets/images/Avatar.jpg';
-import backGround from "../../assets/images/fon.jpg";
+import background from "../../assets/images/fon.jpg";
+import ava01 from "../../assets/images/followers/ava01.jpg";
+import ava02 from "../../assets/images/followers/ava02.jpg";
+import ava03 from "../../assets/images/followers/ava03.png";
+import ava04 from "../../assets/images/followers/ava04.png";
+import Header from "../../components/header/Header";
+import React from "react";
 
-export default function Profile() {
+export default function Profile(): JSX.Element {
   return (
     <>
-      <div className="topBar">Topbar</div>
+      <Header/>
       <div className="profile">
         <div className="sideBar">Sidebar</div>
         <div className="profileBlock">
           <div className="profileBlockTop">
-            <img src={backGround} className="profileFonImg" alt="fon"/>
+            <img src={background} className="profileFonImg" alt="fon"/>
             <img src={userPhoto} className="profileUserImg" alt="user ava"/>
             <div className="profileInfo">
               <h3 className="profileName">Tribis Alexandr</h3>
-              <div className="profileStatus">Follow your heart</div>
+              <div className="profileStatus">"Follow your heart"</div>
             </div>
           </div>
           <div className="profileBlockBottom">
@@ -35,7 +41,27 @@ export default function Profile() {
                   <span className="profileDescriptionValue">Married</span>
                 </div>
               </div>
-              <div className="followers">Followers</div>
+              <div className="followersBlock">
+                <h4 className="profileDescriptionInfo">Followers</h4>
+                <div className="followers">
+                  <div className="follower">
+                    <img className={"followerImg"} src={ava01} alt={"follower 01"}/>
+                    <span className="followerName">Abram Gustov</span>
+                  </div>
+                  <div className="follower">
+                    <img className={"followerImg"} src={ava02} alt={"follower 01"}/>
+                    <span className="followerName">Gregory Chestor</span>
+                  </div>
+                  <div className="follower">
+                    <img className={"followerImg"} src={ava03} alt={"follower 01"}/>
+                    <span className="followerName">Ivan Ivanov</span>
+                  </div>
+                  <div className="follower">
+                    <img className={"followerImg"} src={ava04} alt={"follower 01"}/>
+                    <span className="followerName">Federico Augustino</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
