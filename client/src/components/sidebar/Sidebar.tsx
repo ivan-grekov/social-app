cd922f304a24a7c84dd6188c4533267705864780
LeonID <lmatskevich79@gmail.com>
2023-02-04 08:52:58 +0300

    fix: fix avatar

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,12 +1,61 @@
 import './sidebar.scss';
+import {
+  RssFeed,
+  Chat,
+  VideoLibrary,
+  Groups,
+  Bookmark,
+  QuestionMark,
+  Work,
+  Event,
+  School,
+} from '@mui/icons-material';
 
 export default function Sidebar() {
-
-    return (
-      <div className='sidebar'>
-      <div className="sideWrapper">rrrrr</div>
+  return (
+    <div className="sidebar">
+      <div className="sideWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+            <VideoLibrary className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <Groups className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmarcs</span>
+          </li>
+          <li className="sidebarListItem">
+            <QuestionMark className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <Work className="sidebarIcon" />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
       </div>
-    );
-
+    </div>
+  );
 }
-
