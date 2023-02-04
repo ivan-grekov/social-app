815ac6a53277b164f908a2a5ece9e6448b348213
LeonID <lmatskevich79@gmail.com>
2023-02-04 21:22:18 +0300

    feat: add post and feed

--- client/src/components/feed/Feed.tsx
+++ client/src/components/feed/Feed.tsx
@@ -1,10 +1,22 @@
 import './feed.scss';
+import Share from '../share/Share';
+import Post from '../post/Post';
 
 export default function Feed() {
   return (
-    <div className='feed'>
-feed
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+<Post/>
+<Post/>
+<Post/>
+<Post/>
+<Post/>
+<Post/>
+<Post/>
+<Post/>
+<Post/>
+      </div>
     </div>
   );
 }
-
