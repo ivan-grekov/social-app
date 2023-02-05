0e39726fdb69b06a21cbf129f7ce9d24ef01fdfa
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-05 22:15:20 +0300

    refactor: add profile page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
-// import Home from './pages/home/Home';
-import React from "react";
-import Login from "./pages/login/Login";
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+
 function App(): JSX.Element {
-  return <Login />;
+  return (
+    <>
+      <Profile />
+    </>
+  );
 }
 
 export default App;
