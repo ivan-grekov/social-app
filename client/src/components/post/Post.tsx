0115a0ebb3e6524129508223d5f0c21043ad737d
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-07 20:36:53 +0300

    feat: add functionality to fetch user form api

--- client/src/components/post/Post.tsx
+++ client/src/components/post/Post.tsx
@@ -1,33 +1,48 @@
 import './post.scss';
 import { MoreVert } from '@mui/icons-material';
-import { Users } from '../../static/Data';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { PostProps } from '../../static/types';
+import axios from 'axios';
+import { IUser } from '../../static/types';
+import { format } from 'timeago.js';
+import { Link } from 'react-router-dom';
 
 const Post: React.FC<PostProps> = ({ post }) => {
   const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [like, setLike] = useState(post.likes.length);
+  const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState<IUser>(Object);
 
-  const [like, setLike] = useState(post.like);
-  const [isliked, setIsLiked] = useState(false);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/api/users?userId=${post.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [post.userId]);
 
-  const likeHandler = () => {
-    setLike(isliked ? like - 1 : like + 1);
-    setIsLiked(!isliked);
-  };
+  // const likeHandler = () => {
+  //   setLike(isliked ? like - 1 : like + 1);
+  //   setIsLiked(!isliked);
+  // };
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
-              alt="profile icon"
-              className="postProfileImg"
-            />
-            <span className="postUsername">
-              {Users.filter((u) => u.id === post.userId)[0].username}
-            </span>
-            <span className="postDate">{post.date}</span>
+            <Link to={`profile/${user.username}`}>
+              <img
+                src={
+                  user.profilePicture
+                    ? publicFolder + user.profilePicture
+                    : publicFolder + 'person/noAvatar.png'
+                }
+                alt="profile icon"
+                className="postProfileImg"
+              />
+            </Link>
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -37,7 +52,7 @@ const Post: React.FC<PostProps> = ({ post }) => {
           <span className="postText">{post.desc}</span>
           <img
             className="postImg"
-            src={publicFolder + post.photo}
+            src={publicFolder + post.img}
             alt="post overview"
           />
         </div>
@@ -47,18 +62,16 @@ const Post: React.FC<PostProps> = ({ post }) => {
               className="likeIcon"
               src={`${publicFolder}like.png`}
               alt="like"
-              onClick={likeHandler}
             />
             <img
               className="likeIcon"
               src={`${publicFolder}heart.png`}
               alt="heart"
-              onClick={likeHandler}
             />
             <span className="postLikeCounter">{like} people liked it</span>
           </div>
           <div className="posrBottomRight">
-            <span className="postCommentText">{post.comment} comments</span>
+            <span className="postCommentText">comments</span>
           </div>
         </div>
       </div>
