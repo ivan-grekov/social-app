547cd0aa4af285b78319df2791cecea85bf0f9a3
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-07 09:48:00 +0300

    feat: change path to the content include env

--- client/src/components/onlineFriend/OnlineFriend.tsx
+++ client/src/components/onlineFriend/OnlineFriend.tsx
@@ -3,12 +3,13 @@ import './onlineFriend.scss';
 import { UserProps } from '../../static/types';
 
 const OnlineFriend: React.FC<UserProps> = ({ user }) => {
+  const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <li className="rightbarFriend">
       <div className="rightbarProfileImgContainer">
         <img
           className="rightbarProfileImg"
-          src={user.profilePicture}
+          src={publicFolder + user.profilePicture}
           alt="user icon"
         />
         <span className="rightbarOnline"></span>
