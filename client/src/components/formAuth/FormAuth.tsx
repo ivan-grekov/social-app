fed8626715cce6bb70800a4f74f3ceda9753a1c3
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-07 09:47:45 +0300

    refactor: move interface of props into types file

--- client/src/components/formAuth/FormAuth.tsx
+++ client/src/components/formAuth/FormAuth.tsx
@@ -1,68 +1,73 @@
-import React, {useState} from "react";
-import "./formAuth.scss";
+import React, { useState } from 'react';
+import './formAuth.scss';
+import { propsFormAuth } from '../../static/types';
 
-interface propsFormAuth {
-  title: string;
-  isLogin: boolean;
-  handleClick: void;
-}
-
-const FormAuth = ({title, isLogin, handleClick}: propsFormAuth): JSX.Element => {
+const FormAuth = ({
+  title,
+  isLogin,
+  handleClick,
+}: propsFormAuth): JSX.Element => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [pass, setPass] = useState('');
 
   return (
     <>
-      <form className='formAuth'>
-        <h2 className='formAuthTitle'>{title}</h2>
-        {
-          !isLogin
-            ? <input
-              className='input'
-              placeholder='Enter your name'
-              value={name}
-              onChange={e => setName(e.target.value)}
-            />
-            : null
-        }
+      <form className="formAuth">
+        <h2 className="formAuthTitle">{title}</h2>
+        {!isLogin ? (
+          <input
+            className="input"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        ) : null}
         <input
-          className='input'
-          placeholder='Enter your email'
+          className="input"
+          placeholder="Enter your email"
           value={email}
           type="email"
           required
-          onChange={e => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
-          className='input'
-          placeholder='Enter your password'
+          className="input"
+          placeholder="Enter your password"
           value={pass}
           type="password"
-          onChange={e => setPass(e.target.value)}
+          onChange={(e) => setPass(e.target.value)}
         />
-        {
-          !isLogin
-            ? <input
-              className='input'
-              placeholder='Enter the password again'
-              value={pass}
-              type="password"
-              onChange={e => setPass(e.target.value)}
-            />
-            : null
-        }
-        <div className=''>
-          {
-            isLogin
-              ? <div>No account?<span className="signLink" onClick={() => handleClick}>Sign up</span></div>
-              : <div>Have an account?<span className="signLink" onClick={() => handleClick}>Sign in</span></div>
-          }
+        {!isLogin ? (
+          <input
+            className="input"
+            placeholder="Enter the password again"
+            value={pass}
+            type="password"
+            onChange={(e) => setPass(e.target.value)}
+          />
+        ) : null}
+        <div className="">
+          {isLogin ? (
+            <div>
+              No account?
+              <span className="signLink" onClick={() => handleClick}>
+                Sign up
+              </span>
+            </div>
+          ) : (
+            <div>
+              Have an account?
+              <span className="signLink" onClick={() => handleClick}>
+                Sign in
+              </span>
+            </div>
+          )}
         </div>
-        <button className='buttonAuth button'>{title}</button>
+        <button className="buttonAuth button">{title}</button>
       </form>
     </>
-  )
-}
+  );
+};
 
 export default FormAuth;
