654b1f55dcfa5b7282cb08826faf24317a35dfa8
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-07 09:45:35 +0300

    feat: change path to the content include env

--- client/src/components/closeFriend/CloseFriend.tsx
+++ client/src/components/closeFriend/CloseFriend.tsx
@@ -2,11 +2,12 @@ import './closeFriend.scss';
 import { UserProps } from '../../static/types';
 
 const CloseFriend: React.FC<UserProps> = ({ user }) => {
+  const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <li className="sidebarFriend">
       <img
         className="sidebarFriendImg"
-        src={user.profilePicture}
+        src={publicFolder + user.profilePicture}
         alt="profile icon"
       />
       <span className="sidebarFriendName">{user.username}</span>
