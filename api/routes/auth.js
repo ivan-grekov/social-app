bfe3aaf2b75664190ecf62949e1e0729b7be841f
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-08 21:59:27 +0300

    fix: coorrect response data

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -27,17 +27,17 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).send('user not found');
+    !user && res.status(404).json('user not found');
 
     const validPassword = await bcrypt.compare(
       req.body.password,
       user.password
     );
-    !validPassword && res.status(400).json('password not valid');
+    !validPassword && res.status(400).json('wrong password');
 
     res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
