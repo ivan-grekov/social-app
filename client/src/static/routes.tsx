ed028d6aa762e31e0ac3dc4b075ca2e8767a455e
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-09 23:11:04 +0300

    refactor: delete unused import

--- client/src/static/routes.tsx
+++ client/src/static/routes.tsx
@@ -3,6 +3,7 @@ import Home from '../pages/home/Home';
 import Login from '../pages/login/Login';
 import Register from '../pages/register/Register';
 import Profile from '../pages/profile/Profile';
+import { AuthContext } from '../context/AuthContext';
 
 export const routes = createBrowserRouter([
   {
