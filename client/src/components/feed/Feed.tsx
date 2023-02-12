1c20460674566941024cf8b722e194a97efe0f41
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-12 22:14:22 +0300

    feat: add func to sort posts

--- client/src/components/feed/Feed.tsx
+++ client/src/components/feed/Feed.tsx
@@ -3,8 +3,8 @@ import Share from '../share/Share';
 import Post from '../post/Post';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {IPost, FeedProps, UserContext} from '../../static/types';
-import {AuthContext} from "../../context/AuthContext";
+import { IPost, FeedProps, UserContext } from '../../static/types';
+import { AuthContext } from '../../context/AuthContext';
 
 const Feed: React.FC<FeedProps> = ({ username }) => {
   const [posts, setPosts] = useState([]);
@@ -15,10 +15,16 @@ const Feed: React.FC<FeedProps> = ({ username }) => {
       const res = username
         ? await axios.get(`/api/posts/profile/${username}`)
         : await axios.get(`/api/posts/timeline/${user?._id}`);
-      setPosts(res.data);
+      setPosts(
+        res.data.sort((p1: IPost, p2: IPost) => {
+          return (
+            new Date(p2.createdAt).valueOf() - new Date(p1.createdAt).valueOf()
+          );
+        })
+      );
     };
     fetchPosts();
-  }, [username]);
+  }, [username, user?._id]);
 
   return (
     <div className="feed">
