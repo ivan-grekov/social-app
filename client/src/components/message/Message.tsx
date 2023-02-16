9a97419302e3683da5cb0b4b036f701a3e2070e6
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-16 09:35:14 +0300

    feat: add message component

---
+++ client/src/components/message/Message.tsx
@@ -0,0 +1,26 @@
+import './message.scss';
+import { IMessage } from '../../static/types';
+import { format } from 'timeago.js';
+
+interface MessageProps {
+  own?: boolean;
+  message: IMessage;
+}
+
+const Message = ({ message, own }: MessageProps) => {
+  return (
+    <div className={own ? 'message own' : 'message'}>
+      <div className="messageTop">
+        <img
+          className="messageImg"
+          src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+          alt="user profile picture"
+        />
+        <p className="messageText">{message.text}</p>
+      </div>
+      <div className="messageBottom">{format(message.createdAt)}</div>
+    </div>
+  );
+};
+
+export default Message;
