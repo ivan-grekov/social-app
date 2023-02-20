601631e593b48507319605b38551ecb6425e63b6
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-20 17:02:00 +0300

    fix: correct path to page

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -18,59 +18,55 @@ import { Link } from 'react-router-dom';
 export default function Sidebar() {
   return (
     <div className="sidebar">
-
       <div className="sidebarWrapper">
-
         <div className="sidebarContainer">
-
-
-        <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <RssFeed className="sidebarIcon" />
-            <span className="sidebarListItemText">Feed</span>
-          </li>
-          <Link to={'messenger/'} style={{ color: 'inherit' }}>
+          <ul className="sidebarList">
             <li className="sidebarListItem">
-              <Chat className="sidebarIcon" />
-              <span className="sidebarListItemText">Chats</span>
+              <RssFeed className="sidebarIcon" />
+              <span className="sidebarListItemText">Feed</span>
             </li>
-          </Link>
-          <li className="sidebarListItem">
-            <VideoLibrary className="sidebarIcon" />
-            <span className="sidebarListItemText">Videos</span>
-          </li>
-          <li className="sidebarListItem">
-            <Groups className="sidebarIcon" />
-            <span className="sidebarListItemText">Groups</span>
-          </li>
-          <li className="sidebarListItem">
-            <Bookmark className="sidebarIcon" />
-            <span className="sidebarListItemText">Bookmarcs</span>
-          </li>
-          <li className="sidebarListItem">
-            <QuestionMark className="sidebarIcon" />
-            <span className="sidebarListItemText">Questions</span>
-          </li>
-          <li className="sidebarListItem">
-            <Work className="sidebarIcon" />
-            <span className="sidebarListItemText">Jobs</span>
-          </li>
-          <li className="sidebarListItem">
-            <Event className="sidebarIcon" />
-            <span className="sidebarListItemText">Events</span>
-          </li>
-          <li className="sidebarListItem">
-            <School className="sidebarIcon" />
-            <span className="sidebarListItemText">Courses</span>
-          </li>
-        </ul>
-        <button className="sidebarButton">Show More</button>
-        <hr className="sidebarHr" />
-        <ul className="sidebarFriendList">
-          {Users.map((u) => (
-            <CloseFriend key={u.id} user={u} />
-          ))}
-        </ul>
+            <Link to={'/messenger'} style={{ color: 'inherit' }}>
+              <li className="sidebarListItem">
+                <Chat className="sidebarIcon" />
+                <span className="sidebarListItemText">Chats</span>
+              </li>
+            </Link>
+            <li className="sidebarListItem">
+              <VideoLibrary className="sidebarIcon" />
+              <span className="sidebarListItemText">Videos</span>
+            </li>
+            <li className="sidebarListItem">
+              <Groups className="sidebarIcon" />
+              <span className="sidebarListItemText">Groups</span>
+            </li>
+            <li className="sidebarListItem">
+              <Bookmark className="sidebarIcon" />
+              <span className="sidebarListItemText">Bookmarcs</span>
+            </li>
+            <li className="sidebarListItem">
+              <QuestionMark className="sidebarIcon" />
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+            <li className="sidebarListItem">
+              <Work className="sidebarIcon" />
+              <span className="sidebarListItemText">Jobs</span>
+            </li>
+            <li className="sidebarListItem">
+              <Event className="sidebarIcon" />
+              <span className="sidebarListItemText">Events</span>
+            </li>
+            <li className="sidebarListItem">
+              <School className="sidebarIcon" />
+              <span className="sidebarListItemText">Courses</span>
+            </li>
+          </ul>
+          <button className="sidebarButton">Show More</button>
+          <hr className="sidebarHr" />
+          <ul className="sidebarFriendList">
+            {Users.map((u) => (
+              <CloseFriend key={u.id} user={u} />
+            ))}
+          </ul>
         </div>
       </div>
     </div>
