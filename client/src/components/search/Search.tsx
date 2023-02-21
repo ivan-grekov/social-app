f450469a819b1b2c9534e2e4a000f7012cbb8ee7
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-21 11:23:01 +0300

    feat: add query string into context

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -1,13 +1,23 @@
 import './search.scss';
 import { Search } from '@mui/icons-material';
+import React from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function SearchBar() {
+  const [queryString, setQueryString] = React.useState('');
+  const { dispatch } = React.useContext(AuthContext);
+
+  React.useEffect(() => {
+    dispatch({ type: 'SET_QUERY', payload: queryString });
+  }, [queryString]);
+
   return (
     <div className="searchbar">
       <Search className="searchIcon" />
       <input
         placeholder="Search for friend, post or video"
         className="searchInput"
+        onChange={(e) => setQueryString(e.target.value)}
       />
     </div>
   );
