db9c6a7950d712a8c4244f3d0160558af6e47e08
Ivan Grekov <ivan_grekov@yahoo.com>
2023-02-27 08:59:37 +0300

    refactor: delete import of styles

--- client/src/components/CalendarSidebar/CalendarSidebar.tsx
+++ client/src/components/CalendarSidebar/CalendarSidebar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import EventButton from '../EventButton/EventButton';
-import './calendarSidebar.scss';
 import SmallCalendar from '../SmallCalendar/SmallCalendar';
 import CalendarLabels from '../CalendarLabels/CalendarLabels';
 
